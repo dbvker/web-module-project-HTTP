5f6bc55b4bb4c05661de2c7f6000cb1857e6196e
Dylan Baker <dylanmbaker@yahoo.com>
2022-01-26 21:38:43 -0600

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 import MovieHeader from './components/MovieHeader';
 import FavoriteMovieList from './components/FavoriteMovieList';
 import EditMovieForm from "./components/EditMovieForm";
+import AddMovieForm from "./components/AddMovieForm";
 
 import axios from 'axios';
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const { push } = useHistory();
 
   useEffect(()=>{
     axios.get('http://localhost:9000/api/movies')
@@ -25,6 +27,15 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    axios.delete(`http://localhost:9000/api/movies/${id}`)
+            .then(resp => {
+                console.log(resp.data);
+                setMovies(resp.data);
+                push('/');
+            })
+            .catch(err => {
+                console.error(err);
+            })
   }
 
   const addToFavorites = (movie) => {
@@ -44,16 +55,22 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
-              <EditMovieForm />
+              <EditMovieForm setMovies={setMovies} />
+            </Route>
+            
+            <Route path="/movies/add">
+              <AddMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
               <MovieList movies={movies}/>
             </Route>
+
+            
 
             <Route path="/">
               <Redirect to="/movies"/>
